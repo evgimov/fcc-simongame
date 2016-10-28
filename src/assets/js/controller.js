996f587bbda00f735b13af9ef5a80ece3125866c
evgimov <evgimov@gmail.com>
2016-10-27 21:42:24 -0700

    Added playerMoveHandler method, check if current move is correct and set another level

--- src/assets/js/controller.js
+++ src/assets/js/controller.js
@@ -1,4 +1,5 @@
-//todo: initialize timeouts with null values
+// TODO: check if
+
 (function (window){
     'use strict';
 
@@ -33,7 +34,55 @@
         }else if (id === 'strict-btn'){
 			this.strictHandler();
 		}
+		else {
+            this.playerMoveHandler(id);
+        }
 	};
+
+	Controller.prototype.playerMoveHandler = function(id){
+        var self = this;
+        var tout = null;
+        console.log("Button id:" + id);
+        console.log("Player count:" + this.model.playerCount);
+        console.log("Simon count:" + this.model.simonCount);
+
+        this.view.playGameButton(id);
+        this.view.doButtonBrighter(id);
+        var val = this.model.checkNextMove(id);
+        switch (val) {
+            case 'continue':
+                this.model.playerCount++;
+                console.log("Player:" + this.model.playerCount);
+                break;
+            case 'finish':
+
+                break;
+            case 'error':
+
+                break;
+        }
+
+        if (this.model.playerCount === this.model.simonCount){
+            tout = setTimeout(function () {
+                self.model.simonCount++;
+                self.model.playerCount = 0;
+                console.log("Simon:" + self.model.simonCount);
+                self.view.setScreenText(self.model.getSimonCount());
+                self.view._disableGameButtons();
+                self.showComputerMoves(function (delay) {
+                    setTimeout(function () {
+                        if (!self.view.buttonClickable) {
+                            self.view._enableGameButtons();
+                        }
+                        else {
+
+                        }
+                    }, delay);
+                });
+            },500);
+            this.view.timeouts.push(tout);
+        }
+    };
 
 	Controller.prototype.startHandler = function(){
         var self = this;
@@ -45,7 +94,7 @@
                     if (!self.view.buttonClickable){
                         self.view._enableGameButtons();
                     }
-                },delay);
+                },delay - 750);
             });
         });
     };
